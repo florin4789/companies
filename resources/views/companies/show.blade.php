73c230abfabad6b27d725721cb3cbdf99140c94e
florin478 <florinstrambeanu478@gmail.com>
2022-01-31 11:54:41 +0200

    m3

--- resources/views/companies/show.blade.php
+++ resources/views/companies/show.blade.php
@@ -22,7 +22,7 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Details:</strong>
-                {{ $company->detail }}
+                {{ $company->details }}
             </div>
         </div>
     </div>
